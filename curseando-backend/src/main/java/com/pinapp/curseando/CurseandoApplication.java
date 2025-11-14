7050d9d24c7c782cdf01fafbd07264f116656af2
bolea <bruno.olea@cognirock.com>
2025-11-13 21:38:13 -0300

    Initialize Spring Boot project structure

---
+++ curseando-backend/src/main/java/com/pinapp/curseando/CurseandoApplication.java
@@ -0,0 +1,13 @@
+package com.pinapp.curseando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurseandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurseandoApplication.class, args);
+	}
+
+}
