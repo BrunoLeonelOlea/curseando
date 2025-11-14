75395272a47b1c526a18dc17cd764b1d9be4b6bb
bolea <bruno.olea@cognirock.com>
2025-11-14 17:00:35 -0300

    fix API url

--- curseando-frontend/src/environments/environment.prod.ts
+++ curseando-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://curseando-backend-production.up.railway.app'
+    apiUrl: 'https://curseando-backend-production.up.railway.app/api'
 }
